32457f118c807121c3e742b73a390ea95dec25f0
Peter <p.eckelmann@gmail.com>
2025-06-29 12:47:41 -0500

    Initial commit

---
+++ configurable-nukes/settings.lua
@@ -0,0 +1,31 @@
+require("libs.log.log-settings")
+
+data:extend({
+  {
+    type = "double-setting",
+    name = "configurable-nukes-area-multiplier",
+    setting_type = "startup",
+    order = "cba",
+    default_value = 1,
+    maximum_value = 111,
+    minimum_value = 0.01
+  },
+  {
+    type = "double-setting",
+    name = "configurable-nukes-damage-multiplier",
+    setting_type = "startup",
+    order = "cbb",
+    default_value = 1,
+    maximum_value = 111,
+    minimum_value = 0.01
+  },
+  {
+    type = "double-setting",
+    name = "configurable-nukes-repeat-multiplier",
+    setting_type = "startup",
+    order = "cbc",
+    default_value = 1,
+    maximum_value = 111,
+    minimum_value = 0.01
+  },
+})
